a62224ee8e7a33e9cdffce031b263f3272b68c1c
Anastasia Karpenko <morganaloca@gmail.com>
2018-09-24 16:39:28 +0300

    Added DAO interface

---
+++ app/src/main/java/com/example/karpena2/roomproject/database/MusicDao.java
@@ -0,0 +1,34 @@
+package com.example.karpena2.roomproject.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface MusicDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAlbums(List<Album> albums);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertSongs(List<Song> songs);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void setLinkdAlbumSongs(List<AlbumSong> linkdAlbumSongs);
+
+    @Query("select * from album")
+    List<Album> getAlbums();
+
+    @Query("select * from song")
+    List<Song> getSongs();
+
+    @Delete
+    void deleteAlbum(Album album);
+
+    @Query("select * from song inner join albumsong on song.id = albumsong.song_id where album_id = :albumId")
+    List<Song> getSongsFromAlbum(int albumId);
+}
