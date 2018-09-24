87a28ffb947615d3f4343ec4e0f2de76b4f649c4
Anastasia Karpenko <morganaloca@gmail.com>
2018-09-24 15:25:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/karpena2/roomproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.karpena2.roomproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // add Room database to the project
+    // insert/read data to/from database
+    // add Content Provider to Room
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
