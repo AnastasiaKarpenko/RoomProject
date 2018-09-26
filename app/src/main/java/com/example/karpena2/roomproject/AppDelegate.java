a619f76ca7bb4caf4ebaf59a78bbf31bb0268bf3
Anastasia Karpenko <morganaloca@gmail.com>
2018-09-26 08:49:59 +0300

    Created AppDelegate (extends Application) class and added it to manifest

---
+++ app/src/main/java/com/example/karpena2/roomproject/AppDelegate.java
@@ -0,0 +1,12 @@
+package com.example.karpena2.roomproject;
+
+import android.app.Application;
+
+public class AppDelegate extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
